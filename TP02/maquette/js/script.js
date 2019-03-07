09ae952ba50039aefbfe1c9e9d63967208fb1906
adri <adri@localhost.localdomain>
2019-03-07 17:13:53 +0100

    acceleration

--- TP02/maquette/js/script.js
+++ TP02/maquette/js/script.js
@@ -72,22 +72,125 @@ function Envoyer(formulaire) {
 		alert("maps n'est pas supporté par votre navigateur");
 	    //initMap(46.139023,-2.435386, 6);
 
+function deviceOrientationListener(event) {			
+	var xValue = Math.round(event.gamma);
+	var yValue = Math.round(event.beta);
+	var Rotation = Math.round(event.alpha);
+	alert(xValue);
 
+/*
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+
+	ctx.clearRect(0, 0, c.width, c.height);
+	ctx.fillStyle = "#FF7777";
+	ctx.font = "14px Verdana";
+	ctx.fillText("Alpha: " + Math.Round(event.alpha), 10, 20);
+	ctx.beginPath();
+	ctx.moveTo(180, 75);
+	ctx.lineTo(210, 75);
+	ctx.arc(180, 75, 60, 0, event.alpha * Math.PI / 180);
+	ctx.fill();
+
+	ctx.fillStyle = "#FF6600";
+	ctx.fillText("Beta: " + Math.round(event.beta), 10, 140);
+	ctx.beginPath();
+	ctx.fillRect(180, 150, event.beta, 90);
+
+	ctx.fillStyle = "#FF0000";
+	ctx.fillText("Gamma: " + Math.round(event.gamma), 10, 270);
+	ctx.beginPath();
+	ctx.fillRect(90, 340, 180, event.gamma);
+*/
+}
+if (window.DeviceOrientationEvent) {
+	window.addEventListener("deviceorientation", deviceOrientationListener);
+} else {
+	alert("Device Orientation n'est pas supporté");
+}
+
+
+
+
+
+
+
+if ( !window.requestAnimationFrame ) {
+ 
+    window.requestAnimationFrame = ( function() {
+ 
+        return window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+ 
+            window.setTimeout( callback, 1000 / 60 );
+ 
+        };
+ 
+    } )();
+ 
+}
+
+
+
+var logo;
+var w;
+var h;
 
 function init()
 {
-	if (window.DeviceOrientationEvent) {
-		
-		window.addEventListener("deviceorientation", function(event) 
-		{
-			document.getElementById("y").style.webkitTransform = "scaleY("+(Math.round(event.beta))+")";  
-			document.getElementById("x").style.webkitTransform = "scaleX("+(Math.round(event.gamma))+")";
-			document.getElementById("angle").style.webkitTransform = "rotateZ("+(Math.round(event.alpha))+"deg)";  
-		}, true);
-		
-		
-		
-	} else {
-  	alert("Sorry, your browser doesn't support Device Orientation");
-	} 
+    logo = document.getElementById("logo");
+	w = window.innerWidth;
+    h = window.innerHeight;
+	logo.style.left = (w/2)-50+"px";
+	logo.style.top = (h/2)-50+"px";
+	logo.velocity = {x:0,y:0}
+	logo.position = {x:0,y:0}
+    
+    if (window.DeviceOrientationEvent) {
+		window.addEventListener("deviceorientation", function(event){
+			logo.velocity.y = Math.round(event.beta);
+			logo.velocity.x = Math.round(event.gamma);
+			alert(logo.velocity.y);
+			alert(logo.velocity.x);
+        }
+    )}
+    else {
+	  	alert("Sorry, your browser doesn't support Device Orientation");
+	} ;
+    
 }
+
+function update()
+{
+        logo.position.x += logo.velocity.x;
+        logo.position.y += logo.velocity.y;
+        
+        if(logo.position.x > (w-100) && logo.velocity.x > 0){
+		   logo.position.x = w-100;
+		}
+			
+		if(logo.position.x < 0 && logo.velocity.x < 0){
+			logo.position.x = 0;
+		}
+		
+		if(logo.position.y > (h-100) && logo.velocity.y > 0){
+		   logo.position.y = h-100;
+		}
+		
+		if(logo.position.y < 0 && logo.velocity.y < 0)s{
+		   logo.position.y = 0;
+		}
+    
+    logo.style.top = logo.position.y + "px"
+    logo.style.left = logo.position.x + "px"
+    
+    requestAnimationFrame( update );//KEEP ANIMATING
+}
+
+("#logo").click(function() {
+		init();
+	});
+
